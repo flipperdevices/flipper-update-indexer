7802a366ee10193b97da5b12539bcc33bd11ae70
SG <who.just.the.doctor@gmail.com>
2026-03-02 20:53:28 +0000

    fix settings

--- indexer/src/settings.py
+++ indexer/src/settings.py
@@ -26,8 +26,8 @@ class Settings(BaseModel):
     vgm_github_repo: str
     busybar_github_token: str
     busybar_github_repo: str
-    one_mcu_firmware_github_token: str
-    one_mcu_firmware_github_repo: str
+    flipper_one_mcu_github_token: str
+    flipper_one_mcu_github_repo: str
     private_paths: List[str]
 
 
@@ -54,8 +54,8 @@ settings = Settings(
     vgm_github_repo=os.getenv("INDEXER_VGM_GITHUB_REPO"),
     busybar_github_token=os.getenv("INDEXER_BUSYBAR_GITHUB_TOKEN"),
     busybar_github_repo=os.getenv("INDEXER_BUSYBAR_GITHUB_REPO"),
-    one_mcu_firmware_github_token=os.getenv("INDEXER_ONE_MCU_FIRMWARE_GITHUB_TOKEN"),
-    one_mcu_firmware_github_repo=os.getenv("INDEXER_ONE_MCU_FIRMWARE_GITHUB_REPO"),
+    flipper_one_mcu_github_token=os.getenv("INDEXER_FLIPPER_ONE_MCU_GITHUB_TOKEN"),
+    flipper_one_mcu_github_repo=os.getenv("INDEXER_FLIPPER_ONE_MCU_GITHUB_REPO"),
     private_paths=[
         "reindex",
         "uploadfiles",
